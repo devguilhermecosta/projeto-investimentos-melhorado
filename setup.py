bd1936a2a066fd541079dbe8ae93b91f84cafd51
Guilherme Costa <98500282+devguilhermecosta@users.noreply.github.com>
2022-10-18 21:23:55 -0300

    criei o setup.py

---
+++ setup.py
@@ -0,0 +1,33 @@
+import sys
+
+from cx_Freeze import Executable, setup
+
+build_exe_options: dict = {'packages': ['sqlite3',
+                                        'abc',
+                                        'time',
+                                        'tkinter',
+                                        'ttkthemes',
+                                        ]
+                           }
+
+base: str = ''
+
+if sys.platform == 'win32':
+    base = "Win32GUI"
+
+executables: str = (
+    [
+        Executable("app.py",
+                   base=base,
+                   icon='images/logo.ico',
+                   )
+        ]
+    )
+
+setup(
+    name = 'PyInvest',
+    version = '1.0.0',
+    description = 'Aplicativo para controle de investimentos',
+    options = {'build_exe': build_exe_options},
+    executables = executables,
+)
